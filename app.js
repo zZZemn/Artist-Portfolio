d304192f6e541f58adb6255195593302533783e0
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-09-25 16:04:32 +0800

    project page

--- app.js
+++ app.js
@@ -28,6 +28,17 @@ const navBGshow = () => {
     });
 }   //Scroll then add in the classlist
 
+const topFunction = () => {
+    document.body.scrollTop = 0; //Safari
+    document.documentElement.scrollTop = 0; //Chrome, Firefox, IE and Opera
+}
+
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg"];
+
+for (let x = 0; x < images.length; x++) {            
+    const element = '<img src="/Images/"' + images[x] + '">'
+    document.querySelector('.projects-container').innerHTML += element;
+}
 
 const functionCall = () => {
 navBGshow();
